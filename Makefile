836aec89c039dc8c3962a6f97ade768fca4d0184
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-12-01 17:26:00 +0900

    initial commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+all:
+	@pbpaste > ascii.txt
+	@cat ascii.txt| sed 's/,/\t/g' | sh conv.sh
+
+json:
+	@pbpaste > ascii.txt
+	@cat ascii.txt| sed 's/,/\t/g' | sh conv.sh | jq .
---
+++ conv.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/sh
+
+if [ -t 0 ] ; then
+	string="$1"
+else
+	string="$(cat -)"
+fi
+
+IFS='\t'
+result=""
+
+for ix in $string; do
+	hex=$(printf "%x" $ix)
+	char=$(printf "\x${hex}")
+	conv=$(printf "%c" $char)
+	result=$result$conv
+done
+
+echo $result
